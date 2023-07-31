2e99666951126db5101bfbb142fa61895c346785
nanabright775 <nanabrightatsiatorme@gmail.com>
2023-08-01 04:29:51 +0600

    second commit

--- Django_React Project/music_controller/api/views.py
+++ Django_React Project/music_controller/api/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework import generics
+from .serializers import RoomSerializers
+from .models import Room
+#create your views here
 
-
-
-def main(request):
-    return HttpResponse("helo")
-# Create your views here.
+class RoomView(generics.ListAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializers
